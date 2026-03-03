fe40b6dbdb3e3ff43914911269e2a5c14077f409
mobelite <ymbarek@mobelite.fr>
2026-03-04 00:57:54 +0100

    chore: initialize project configuration

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,31 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    plugins: [
+      '@babel/plugin-transform-export-namespace-from',
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            _env: './src/config/env',
+            _config: './src/config',
+            _components: './src/components/',
+            _screens: './src/screens/',
+            _adapters: './src/adapters/',
+            _enums: './src/enums/',
+            _utils: './src/utils/',
+            _navigation: './src/navigation',
+            _hooks: './src/hooks/',
+            _languages: './src/locales/languages',
+            _assets: './src/assets',
+            _i18n: './src/locales/i18n',
+            _helpers: './src/helpers',
+            _validation: './src/validation',
+          },
+        },
+      ],
+    ],
+    presets: ['module:@react-native/babel-preset'],
+  };
 };
