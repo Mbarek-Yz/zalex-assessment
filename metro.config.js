04970b59961701ac6a4e0919dca9ed3382ce6bd6
mobelite <ymbarek@mobelite.fr>
2026-03-05 00:02:41 +0100

    chore: update metro config to support pdf assets

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,6 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require('@react-native/metro-config');
+const config = getDefaultConfig(__dirname);
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+config.resolver.assetExts.push('pdf');
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = config;
